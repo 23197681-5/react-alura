f95f4575ebea7f20faa17e9c7a4652e6a0daf1b9
Willian Alberto Lauber <e018194@celesc.com.br>
2022-01-27 11:09:58 -0300

    feat: note creation

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -2,14 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import NoteList from "./components/noteList/index";
 import RegistrationForm from "./components/registrationform/index";
+import { Component } from "react";
 
-function App() {
-  return (
-    <section>
-      <RegistrationForm></RegistrationForm>
-      <NoteList></NoteList>
-    </section>
-  );
+class App extends Component {
+  createNote(title, text) {}
+  render() {
+    return (
+      <section>
+        <RegistrationForm createNote={this.createNote}></RegistrationForm>
+        <NoteList></NoteList>
+      </section>
+    );
+  }
 }
 
 export default App;
--- ceep/src/components/registrationform/registrationForm.js
+++ ceep/src/components/registrationform/registrationForm.js
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
 export class RegistrationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.title = "";
+    this.text = "";
+  }
+
+  _handleTitleChange(event) {
+    event.stopPropagation();
+    this.title = event.target.value;
+  }
+
+  _handleTextChange(event) {
+    event.stopPropagation();
+    this.text = event.target.value;
+  }
+
+  _createNote(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.props.createNote(this.title, this.text);
+  }
+
   render() {
     return (
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 grid place-items-center v-screen">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 grid place-items-center v-screen"
+        onSubmit={this._createNote(Event).bind(this)}
+      >
         <input
           type="text"
           className="block text-gray-700 text-sm font-bold mb-2"
           placeholder="Annotation title"
+          onChange={this._hangleTitleChange(Event).bind(this)}
         ></input>
-        <textarea rows={3} placeholder="Annotation text"></textarea>
+        <textarea
+          rows={3}
+          placeholder="Annotation text"
+          onChange={this._hangleTextChange(Event).bind(this)}
+        ></textarea>
         <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Create annotation
         </button>
